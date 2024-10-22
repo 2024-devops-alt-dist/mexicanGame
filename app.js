7627760d27bbd14660110815773592f29af87da0
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2024-10-22 16:00:24 +0200

    (feat) img: Create canvas and add images

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 // Le jeu propose 3 niveaux de difficulté thématiques :
 
 // ¡Chiquito! (facile)
@@ -8,6 +6,41 @@
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+let score = 0;
 
 ctx.fillStyle = "green";
 ctx.fillRect(10, 100, 150, 100);
+
+const pinata = new Image(100, 100);
+pinata.src = "img/pinata.png";
+
+const cactus = new Image(100, 100);
+cactus.src = "img/cactus.png";
+
+const calavera = new Image(100, 100);
+calavera.src = "img/calavera.png";
+
+const randomElements = [pinata, cactus, calavera];
+
+function randomImage(array) {
+    const i = Math.floor(Math.random() * array.length);
+    return array[i];
+    //clear canvas
+    //draw image on canvas
+}
+
+function randomCoord() {
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    return [x, y];
+}
+
+console.log(randomImage(randomElements));
+
+
+//score
+// document.getElementById('score-board').addEventListener('click', function(){
+//     score=)
+
+//time
+
