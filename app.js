6e6e3945fcfc1665f85590517a69923d75b2761e
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2024-10-22 15:10:01 +0200

    Set up project

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+// Le jeu propose 3 niveaux de difficulté thématiques :
+
+// ¡Chiquito! (facile)
+// ¡Valiente! (intermédiaire)
+// ¡Luchador! (difficile)
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 100, 150, 100);
