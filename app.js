63e183db57126e6b58f9dda3f327baee6f032393
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2024-10-23 11:07:09 +0200

    (feat) game: Add score, timer and gameplay(click imgs)

--- app.js
+++ app.js
@@ -4,6 +4,16 @@
 // ¡Valiente! (intermédiaire)
 // ¡Luchador! (difficile)
 
+const start = document.getElementById('start')
+
+function launch() {
+    start.addEventListener("click", () => {
+        timer();
+        changeObject();
+        start.style.display = 'none';
+    })
+}
+
 class Object {
     constructor(name, src, points) {
         this.name = name;
@@ -12,51 +22,75 @@ class Object {
     }
 }
 
+let 
+
 const pinata = new Object("pinata", "img/pinata.png", 1);
-const cactus = new Object("cactus", "img/cactus.png", -1);
-const calavera = new Object("pinata", "img/calavera.png", 1);
-
-
-   
-
-
+const cactus = new Object("cactus", "img/cactus.png", -2);
+const calavera = new Object("pinata", "img/calavera.png", 5);
 
 const randomElements = [pinata, cactus, calavera];
+let img = new Image(100, 100);
+let imgObject = {};
+// Variable pour stocker l'intervalle de lempset intervalId;
 
+/**
+ * Chose a random object in the array
+ */
 function randomImage(array) {
     const i = Math.floor((Math.random() * array.length));
-    console.log(i)
     return array[i];
-
-    
-    //clear canvas
-    //draw image on canvas
 }
 
 function randomCoord() {
     return ((Math.random() * 100) + "%");
 }
 
-let img = randomImage(randomElements);
+function timer(){
+    let sec = 30;
+    let timer = setInterval(() => {
+        document.getElementById('safeTimerDisplay').innerHTML = '00:' + sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
 
-const container = document.getElementById("container");
-img.classList.add("img")
-container.appendChild(img);
-console.log(document.querySelector(".img").style.left = randomCoord());
-document.querySelector(".img").style.top = randomCoord();
+/**
+ * generate object called every 3 seconds
+ */
+function changeObject() {
+  // check if an interval has already been set up
+  intervalId = setInterval(generateObject, 3000);
+  }
+
+/**
+ * generate a new Object with a new position 
+ */
+function generateObject() {
+    imgObject = randomImage(randomElements);
+    img.src = imgObject.src;
+    const container = document.getElementById("container");
+    img.classList.add("img");
+    container.appendChild(img);
+    document.querySelector(".img").style.left = randomCoord();
+    document.querySelector(".img").style.top = randomCoord();
+}
+
+// score
+let score = 0;
+let scoreBoard = document.getElementById("score-board");
+
+img.addEventListener("click", function() {
+    score += imgObject.points;
+    if(scoreBoard) {
+        scoreBoard.textContent = "Score: " + score;
+    }
+    container.removeChild(img);
+})
 
 
 
 
 
-
-//score
-// document.getElementById('score-board').addEventListener('click', function(){
-//     if(clickedImg){
-
-//         if('')
-//     }
-//     )}
-
-//time
 
